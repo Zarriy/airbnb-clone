4163deff92d4a78d69f4f4156f964fe1d27eaef2
zarriy <zarriy@outlook.com>
2023-02-06 22:53:58 +0500

    alomst done

--- components/header.js
+++ components/header.js
@@ -1,17 +1,58 @@
+import { useState } from "react";
 import Image from "next/image";
 import logo from "../public/airbnb-logo.png";
 import {
-  MenuIcon,
+  UsersIcon,
   GlobeAltIcon,
   MagnifyingGlassIcon,
   UserCircleIcon,
   Bars3Icon,
 } from "@heroicons/react/24/solid";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRangePicker } from "react-date-range";
+import { useRouter } from "next/router";
 
-export default function Header() {
+export default function Header({ placeholder }) {
+  const [search, setSearch] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [guests, setGuests] = useState(1);
+  const router = useRouter();
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const resetSearch = () => {
+    setSearch("");
+  };
+
+  const searchRouter = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: search,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        guests,
+      },
+    });
+  };
+
+  const selectionRange = {
+    startDate,
+    endDate,
+    key: "selection",
+  };
+
   return (
     <header className="sticky top-0 z-50 bg-white shadow-md grid grid-cols-3 p-5 md:px-10">
-      <div className="relative h-10 flex items-center cursor-pointer">
+      <div
+        className="relative h-10 flex items-center cursor-pointer"
+        onClick={() => router.push("/")}
+      >
         <Image
           src={logo}
           alt="airbnb logo"
@@ -22,9 +63,11 @@ export default function Header() {
       </div>
       <div className="flex items-center border-2 rounded-full shadow-sm py-2">
         <input
-          placeholder="Start your search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder={placeholder || "Start your search"}
           className="pl-5 bg-transparent outline-none flex-grow text-gray-600 placeholder-slate-400 w-full sm:w-auto"
-        ></input>
+        />
         <MagnifyingGlassIcon className="cursor-pointer md:mx-2 h-8 bg-red-400 rounded-full p-1 text-white hidden md:inline-flex" />
       </div>
 
@@ -36,6 +79,37 @@ export default function Header() {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+      {search && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5b61"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              value={guests}
+              onChange={(e) => setGuests(e.target.value)}
+              min={1}
+              types="number"
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+          <div className="flex">
+            <button onClick={resetSearch} className="flex-grow text-gray-500">
+              Cancel
+            </button>
+            <button onClick={searchRouter} className="flex-grow text-red-400">
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
---
+++ pages/search.js
@@ -0,0 +1,66 @@
+import Head from "next/head";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import { useRouter } from "next/router";
+import format from "date-fns/format";
+import InfoCard from "@/components/infoCard";
+
+export default function Search({ searchResults }) {
+  const router = useRouter();
+  const { location, startDate, endDate, guests } = router.query;
+
+  const startDateFormatted = format(new Date(startDate), "dd MMMM yy");
+  const endDateFormatted = format(new Date(endDate), "dd MMMM yy");
+  const range = `${startDateFormatted} - ${endDateFormatted}`;
+
+  return (
+    <>
+      <Head>
+        <title>airbnb Search</title>
+        <meta name="description" content="Searching your desired location" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/airbnb.ico" />
+      </Head>
+      <Header
+        placeholder={`${location} | ${startDateFormatted} - ${endDateFormatted} | ${guests}`}
+      />
+
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-sm">{range}</p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
+
+          <div className="hidden md:inline-flex space-x-3 mb-5 text-gray-800 whitespace-nowrap">
+            <p className="button">Cancelation Flexibility</p>
+            <p className="button">Types of Places</p>
+            <p className="button">Prices</p>
+            <p className="button">Rooms</p>
+            <p className="button">More Filters</p>
+          </div>
+
+          <div className="flex flex-col">
+            {searchResults.map((item, i) => (
+              <InfoCard {...item} key={i} />
+            ))}
+          </div>
+        </section>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const searchResults = await fetch("https://www.jsonkeeper.com/b/5NPS").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      searchResults,
+    },
+  };
+}
